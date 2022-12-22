38f2fb6091a78a402fb248d53b033b2c7f94060b
0nhc <thefoxfoxfox@outlook.com>
2022-12-22 06:23:13 +0000

    mini_pupper_navigation slam and localization passed

--- mini_pupper_navigation/launch/navigate.launch.py
+++ mini_pupper_navigation/launch/navigate.launch.py
@@ -8,20 +8,17 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    use_sim_time = LaunchConfiguration('use_sim_time', default = 'false')
     
-    cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', default=os.path.join(
-                                                  get_package_share_directory('mini_pupper_navigation'), 'config/cartographer'))
+    cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', default = os.path.join( get_package_share_directory('mini_pupper_navigation'), 'config/cartographer'))
                                                   
-    configuration_basename = LaunchConfiguration('configuration_basename',
-                                                 default='nav.lua')
+    configuration_basename = LaunchConfiguration('configuration_basename', default = 'nav.lua')
     
-    load_state_filename = LaunchConfiguration('configuration_basename', default=os.path.join(
-                                                  get_package_share_directory('mini_pupper_navigation'), 'maps/mymap.pbstream'))
+    load_state_filename = LaunchConfiguration('load_state_filename', default = os.path.join(get_package_share_directory('mini_pupper_navigation'), 'maps', 'mymap.pbstream'))
                                                  
-    resolution = LaunchConfiguration('resolution', default='0.05')
+    resolution = LaunchConfiguration('resolution', default = '0.05')
     
-    publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
+    publish_period_sec = LaunchConfiguration('publish_period_sec', default = '1.0')
     
     rviz_config_dir = os.path.join(get_package_share_directory('mini_pupper_navigation'), 'rviz', 'cartographer.rviz')
 
@@ -36,23 +33,23 @@ def generate_launch_description():
         
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value=use_sim_time,
-            description='use_sim_time'),
+            default_value = use_sim_time,
+            description = 'use_sim_time'),
         
         DeclareLaunchArgument(
             'cartographer_config_dir',
-            default_value=cartographer_config_dir,
-            description='Full path to config file to load'),
+            default_value = cartographer_config_dir,
+            description = 'Full path to config file to load'),
         
         DeclareLaunchArgument(
             'configuration_basename',
-            default_value=configuration_basename,
-            description='Name of lua file for cartographer'),
+            default_value = configuration_basename,
+            description = 'Name of lua file for cartographer'),
         
         DeclareLaunchArgument(
             'load_state_filename',
-            default_value=load_state_filename,
-            description='pbstream file'),
+            default_value = load_state_filename,
+            description = 'pbstream file'),
         
         Node(
             package = 'cartographer_ros',
@@ -68,13 +65,13 @@ def generate_launch_description():
         
         DeclareLaunchArgument(
             'resolution',
-            default_value=resolution,
-            description='resolution'),
+            default_value = resolution,
+            description = 'resolution'),
         
         DeclareLaunchArgument(
             'publish_period_sec',
-            default_value=publish_period_sec,
-            description='publish_period_sec'),
+            default_value = publish_period_sec,
+            description = 'publish_period_sec'),
         
         Node(
             package = 'cartographer_ros',
@@ -84,27 +81,27 @@ def generate_launch_description():
                 {'-resolution': resolution},
                 {'-publish_period_sec': publish_period_sec}]),
         
-        # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='base_footprint_to_base_laser_ld06',
-        #     arguments=['0','0','0','0','0','0','base_footprint','base_laser']
-        # ),
+        Node(
+            package = 'tf2_ros',
+            executable = 'static_transform_publisher',
+            name = 'base_footprint_to_base_laser_ld06',
+            arguments = ['0', '0', '0', '0', '0', '0', 'base_footprint', 'base_laser']
+        ),
         
         # Node(
-        #     package='nav2_lifecycle_manager',
-        #     executable='lifecycle_manager',
-        #     name='nav2_lifecycle_manager',
-        #     output='screen',
-        #     parameters=[{'use_sim_time': use_sim_time},
+        #     package = 'nav2_lifecycle_manager',
+        #     executable = 'lifecycle_manager',
+        #     name = 'nav2_lifecycle_manager',
+        #     output = 'screen',
+        #     parameters = [{'use_sim_time': use_sim_time},
         #                 {'autostart': True},
         #                 {'node_names': ['map_server']}]),
         
         Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
+            package = 'rviz2',
+            executable = 'rviz2',
+            name = 'rviz2',
+            arguments = ['-d', rviz_config_dir],
+            parameters = [{'use_sim_time': use_sim_time}],
+            output = 'screen'),
     ])
--- mini_pupper_navigation/launch/slam.launch.py
+++ mini_pupper_navigation/launch/slam.launch.py
@@ -8,40 +8,33 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    use_sim_time = LaunchConfiguration('use_sim_time', default = 'false')
     
-    cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', default=os.path.join(
-                                                  get_package_share_directory('mini_pupper_navigation'), 'config/cartographer'))
+    cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', default = os.path.join( get_package_share_directory('mini_pupper_navigation'), 'config/cartographer'))
                                                   
-    configuration_basename = LaunchConfiguration('configuration_basename',
-                                                 default='slam.lua')
+    configuration_basename = LaunchConfiguration('configuration_basename', default = 'slam.lua')
                                                  
-    resolution = LaunchConfiguration('resolution', default='0.05')
+    resolution = LaunchConfiguration('resolution', default = '0.05')
     
-    publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
+    publish_period_sec = LaunchConfiguration('publish_period_sec', default = '1.0')
     
     rviz_config_dir = os.path.join(get_package_share_directory('mini_pupper_navigation'), 'rviz', 'cartographer.rviz')
 
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value=use_sim_time,
-            description='use_sim_time'),
+            default_value = use_sim_time,
+            description = 'use_sim_time'),
         
         DeclareLaunchArgument(
             'cartographer_config_dir',
-            default_value=cartographer_config_dir,
-            description='Full path to config file to load'),
+            default_value = cartographer_config_dir,
+            description = 'Full path to config file to load'),
         
         DeclareLaunchArgument(
             'configuration_basename',
-            default_value=configuration_basename,
-            description='Name of lua file for cartographer'),
-        
-        DeclareLaunchArgument(
-            'load_state_filename',
-            default_value=load_state_filename,
-            description='pbstream file'),
+            default_value = configuration_basename,
+            description = 'Name of lua file for cartographer'),
         
         Node(
             package = 'cartographer_ros',
@@ -56,13 +49,13 @@ def generate_launch_description():
         
         DeclareLaunchArgument(
             'resolution',
-            default_value=resolution,
-            description='resolution'),
+            default_value = resolution,
+            description = 'resolution'),
         
         DeclareLaunchArgument(
             'publish_period_sec',
-            default_value=publish_period_sec,
-            description='publish_period_sec'),
+            default_value = publish_period_sec,
+            description = 'publish_period_sec'),
         
         Node(
             package = 'cartographer_ros',
@@ -72,18 +65,18 @@ def generate_launch_description():
                 {'-resolution': resolution},
                 {'-publish_period_sec': publish_period_sec}]),
         
-        # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='base_footprint_to_base_laser_ld06',
-        #     arguments=['0','0','0','0','0','0','base_footprint','base_laser']
-        # ),
+        Node(
+            package = 'tf2_ros',
+            executable = 'static_transform_publisher',
+            name = 'base_footprint_to_base_laser_ld06',
+            arguments = ['0', '0', '0', '0', '0', '0', 'base_footprint', 'base_laser']
+        ),
         
         Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
+            package = 'rviz2',
+            executable = 'rviz2',
+            name = 'rviz2',
+            arguments = ['-d', rviz_config_dir],
+            parameters = [{'use_sim_time': use_sim_time}],
+            output = 'screen'),
     ])
