0e6e55bbfe06091ae39a7ce33e3a5bded1fb1f33
YLFeng <ericc_feng@qq.com>
2023-01-01 15:42:30 +0800

    Rename class using CamelCase
    Co-authored-by: Daisuke Sato <tiryoh@gmail.com>

--- mini_pupper_control/mini_pupper_control/servo_interface.py
+++ mini_pupper_control/mini_pupper_control/servo_interface.py
@@ -31,7 +31,7 @@ from trajectory_msgs.msg import JointTrajectory
 from MangDang.mini_pupper.HardwareInterface import HardwareInterface
 
 
-class SERVO_INTERFACE(Node):
+class ServoInterface(Node):
     def __init__(self):
         super().__init__('servo_interface')
         self.subscriber = self.create_subscription(
@@ -64,7 +64,7 @@ class SERVO_INTERFACE(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    servo_interface_node = SERVO_INTERFACE()
+    servo_interface_node = ServoInterface()
     rclpy.spin(servo_interface_node)
     rclpy.shutdown()
 
