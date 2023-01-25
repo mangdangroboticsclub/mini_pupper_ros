645300d0fd9d2a2903d88bc294aae00684bf75f8
Daisuke Sato <tiryoh@gmail.com>
2023-01-26 02:27:55 +0900

    Cleanup packages and update README (#40)

--- README.md
+++ README.md
@@ -1,51 +1,113 @@
-# Mini Pupper ROS2
+# Mini Pupper ROS 2
+
+Supported versions
+
+* Ubuntu 22.04 + ROS 2 Humble
 
 ## 1. Installation
-Ubuntu 22.04 + ROS2 Humble is required. Please follow the [installation document for ROS Humble](https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html)
+
+* To use Gazebo simulator, "1.1 PC Setup" is required.
+* To control Mini Pupper, "1.2 Mini Pupper Setup" is required.
+* To control Mini Pupper using visualize tools, "1.1 PC Setup" and "1.2 Mini Pupper Setup" is required.
+
+### 1.1 PC Setup
+
+PC Setup corresponds to PC (your desktop or laptop PC) for controlling Mini Pupper remotely or execute simulator.  
+__Do not apply these PC Setup commands to your Raspberry Pi on Mini Pupper.__
+
+Ubuntu 22.04 + ROS 2 Humble is required.  
+Please follow the [installation document for ROS Humble](https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html) or use the [unofficial ROS 2 installation script](https://github.com/Tiryoh/ros2_setup_scripts_ubuntu).
+
+After ROS 2 installation, download the Mini Pupper ROS package in the workspace.
 
 ```sh
-cd ~
-mkdir -p ros2_ws/src
-cd ros2_ws/src
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws/src
 git clone https://github.com/mangdangroboticsclub/mini_pupper_ros.git -b ros2
-git clone --recursive https://github.com/chvmp/champ -b ros2
-cd ..
+vcs import < mini_pupper_ros/.minipupper.repos --recursive
+```
+
+Build and install all ROS packages.
+
+```sh
+cd ~/ros2_ws
 rosdep install --from-paths src --ignore-src -r -y
-colcon build --symlink-install
 sudo apt-get install ros-humble-teleop-twist-keyboard
+colcon build --symlink-install
 ```
 
-## 2. Test in RViz
+### 1.2 Mini Pupper Setup
+
+Mini Pupper Setup corresponds to the Raspberry Pi on your Mini Pupper.  
+Ubuntu 22.04 is required.
+
+You should first install dependencies of servos, battery moniter and display screen.  
+See [mini_pupper_bsp](https://github.com/mangdangroboticsclub/mini_pupper_bsp).
+
+After installing the driver software, install ROS 2. ROS 2 Humble is required.  
+Please follow the [installation document for ROS Humble](https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html) or use the [unofficial ROS 2 installation script](https://github.com/Tiryoh/ros2_setup_scripts_ubuntu).
+
+After ROS 2 installation, download the Mini Pupper ROS package in the workspace.
+
+```sh
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws/src
+git clone https://github.com/mangdangroboticsclub/mini_pupper_ros.git -b ros2
+vcs import < mini_pupper_ros/.minipupper.repos --recursive
+# compiling gazebo and cartographer on Raspberry Pi is not recommended
+touch champ/champ/champ_gazebo/AMENT_IGNORE
+touch champ/champ/champ_navigation/AMENT_IGNORE
+touch mini_pupper_ros/mini_pupper_gazebo/AMENT_IGNORE
+touch mini_pupper_ros/mini_pupper_navigation/AMENT_IGNORE
+```
+
+Build and install all ROS packages.
+
+If the Raspberry Pi has less than 4GB memory, try `MAKEFLAGS=-j1 colcon build --executor sequential --symlink-install` instead of `colcon build --symlink-install`
+
+```sh
+# install dependencies without unused heavy packages
+cd ~/ros2_ws
+rosdep install --from-paths src --ignore-src -r -y --skip-keys=joint_state_publisher_gui --skip-keys=rviz2 --skip-keys=gazebo_plugins --skip-keys=velodyne_gazebo_plugins
+sudo apt-get install ros-humble-teleop-twist-keyboard
+colcon build --symlink-install
+```
+
+## 2. Quick Start
+
+## 2.1 PC
+### 2.1.1 Test in RViz
+
+Note: This step is only for PC
+
 ```sh
 # Terminal 1
 . ~/ros2_ws/install/setup.bash # setup.zsh if you use zsh instead of bash
-ros2 launch mini_pupper_bringup bringup.launch.py
+ros2 launch mini_pupper_bringup bringup.launch.py rviz:=true
 
 # Terminal 2
-. ~/ros2_ws/install/setup.bash
-rviz2 -d src/champ/champ_description/rviz/urdf_viewer.rviz
-
-# Terminal 3
 ros2 run teleop_twist_keyboard teleop_twist_keyboard
-# Then control robot dog with your keyboard
+# Then control robot dog with the keyboard
 ```
 
-## 3. Test in Gazebo
+### 2.1.2 Test in Gazebo
+
+Note: This step is only for PC
+
 ```sh
 # Terminal 1
 . ~/ros2_ws/install/setup.bash # setup.zsh if you use zsh instead of bash
-ros2 launch mini_pupper_gazebo gazebo.launch.py
+ros2 launch mini_pupper_gazebo gazebo.launch.py rviz:=true
 
 # Terminal 2
-. ~/ros2_ws/install/setup.bash
-rviz2 -d src/champ/champ_description/rviz/urdf_viewer.rviz
-
-# Terminal 3
 ros2 run teleop_twist_keyboard teleop_twist_keyboard
-# Then control robot dog with your keyboard
+# Then control robot dog with the keyboard
 ```
 
-## 4. Cartographer Test in Gazebo
+### 2.1.3 Cartographer Test in Gazebo
+
+Note: This step is only for PC
+
 ```sh
 # Terminal 1
 . ~/ros2_ws/install/setup.bash
@@ -59,3 +121,14 @@ ros2 launch mini_pupper_navigation slam.launch.py use_sim_time:=true
 ros2 run teleop_twist_keyboard teleop_twist_keyboard
 # Then control robot dog with your keyboard
 ```
+
+## 2.2 Mini Pupper
+
+(To be prepared)
+
+## FAQ
+
+* Q. Is Ubuntu 20.04 supported?
+  * A. No. Ubuntu 22.04 only for now.
+* Q. Is ROS 2 Foxy/Galactic supported?
+  * A. No. ROS 2 Humble only for now.
--- mini_pupper_gazebo/launch/gazebo.launch.py
+++ mini_pupper_gazebo/launch/gazebo.launch.py
@@ -1,10 +1,39 @@
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (c) 2022 MangDang
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# This program is based on https://github.com/champ/champ.
+# which is released under the BSD-3-Clause License.
+# https://spdx.org/licenses/BSD-3-Clause.html
+#
+# Copyright (c) 2019-2020 Juan Miguel Jimeno
+#
+# https://github.com/chvmp/champ/blob/f76d066d8964c8286afbcd9d5d2c08d781e85f54/champ_gazebo/launch/gazebo.launch.py
+
 import os
-import launch_ros
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -27,119 +56,121 @@ def generate_launch_description():
     description_path = os.path.join(
         description_package, 'urdf', 'mini_pupper_description.urdf.xacro')
     default_world_path = os.path.join(
-        gazebo_package, 'worlds', 'default.world')
+        gazebo_package, 'worlds', 'outdoor.world')
 
-    bringup_path = os.path.join(
+    bringup_launch_path = os.path.join(
         bringup_package, 'launch', 'bringup.launch.py')
 
-    champ_bringup_path = os.path.join(
-        get_package_share_directory('champ_bringup'),
-        'launch',
-        'bringup.launch.py'
+    robot_name = LaunchConfiguration("robot_name")
+    sim = LaunchConfiguration("sim")
+    hardware_connected = LaunchConfiguration("hardware_connected")
+    rviz = LaunchConfiguration("rviz")
+    lite = LaunchConfiguration("lite")
+    world = LaunchConfiguration("world"),
+    world_init_x = LaunchConfiguration("world_init_x"),
+    world_init_y = LaunchConfiguration("world_init_y"),
+    world_init_heading = LaunchConfiguration("world_init_heading"),
+    gui = LaunchConfiguration("gui"),
+
+    declare_rviz = DeclareLaunchArgument(
+        name="rviz",
+        default_value="false",
+        description="Launch rviz"
+    )
+    declare_robot_name = DeclareLaunchArgument(
+        name="robot_name",
+        default_value="mini_pupper",
+        description="Robot name"
+    )
+    declare_lite = DeclareLaunchArgument(
+        name="lite",
+        default_value="false",
+        description="Lite"
+    )
+    declare_ros_control_file = DeclareLaunchArgument(
+        name="ros_control_file",
+        default_value=ros_control_config_path,
+        description="ROS control config path",
+    )
+    declare_world = DeclareLaunchArgument(
+        name="world",
+        default_value=default_world_path,
+        description="Gazebo world path"
+    )
+    declare_gui = DeclareLaunchArgument(
+        name="gui",
+        default_value="true",
+        description="Use gui"
+    )
+    declare_world_init_x = DeclareLaunchArgument(
+        name="world_init_x",
+        default_value="0.0"
+    )
+    declare_world_init_y = DeclareLaunchArgument(
+        name="world_init_y",
+        default_value="0.0"
+    )
+    declare_world_init_heading = DeclareLaunchArgument(
+        name="world_init_heading",
+        default_value="0.6"
+    )
+    declare_sim = DeclareLaunchArgument(
+        name='sim',
+        default_value='true',
+        description='Enable use_sime_time to true'
+    )
+    declare_hardware_connected = DeclareLaunchArgument(
+        name='hardware_connected',
+        default_value='false',
+        description='Set to true if connected to a physical robot'
+    )
+
+    mini_pupper_bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(bringup_launch_path),
+        launch_arguments={
+            "use_sim_time": sim,
+            "robot_name": robot_name,
+            "gazebo": sim,
+            "rviz": rviz,
+            "hardware_connected": hardware_connected,
+            "publish_foot_contacts": "true",
+            "close_loop_odom": "true",
+            "joint_controller_topic": "joint_group_effort_controller/joint_trajectory",
+            "joints_map_path": joints_config_path,
+            "links_map_path": links_config_path,
+            "gait_config_path": gait_config_path,
+            "description_path": description_path,
+            "ros_control_file": ros_control_config_path
+        }.items(),
+    )
+    champ_gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory("champ_gazebo"), "launch", "gazebo.launch.py")),
+        launch_arguments={
+            "use_sim_time": sim,
+            "robot_name": robot_name,
+            "world": world,
+            "lite": lite,
+            "world_init_x": world_init_x,
+            "world_init_y": world_init_y,
+            "world_init_heading": world_init_heading,
+            "gui": gui,
+            "close_loop_odom": "true",
+        }.items(),
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            name='use_sim_time',
-            default_value='true',
-            description='Use simulation (Gazebo) clock if true'
-        ),
-
-        DeclareLaunchArgument(
-            name="rviz",
-            default_value="false",
-            description="Launch rviz"
-        ),
-
-        DeclareLaunchArgument(
-            name="robot_name",
-            default_value="mini_pupper",
-            description="Robot name"
-        ),
-
-        DeclareLaunchArgument(
-            name="lite",
-            default_value="false",
-            description="Lite"
-        ),
-
-        DeclareLaunchArgument(
-            name="ros_control_file",
-            default_value=ros_control_config_path,
-            description="ROS control config path",
-        ),
-
-        DeclareLaunchArgument(
-            name="world",
-            default_value=default_world_path,
-            description="Gazebo world path"
-        ),
-
-        DeclareLaunchArgument(
-            name="gui",
-            default_value="true",
-            description="Use gui"
-        ),
-
-        DeclareLaunchArgument(
-            name="world_init_x",
-            default_value="0.0"
-        ),
-
-        DeclareLaunchArgument(
-            name="world_init_y",
-            default_value="0.0"
-        ),
-
-        DeclareLaunchArgument(
-            name="world_init_heading",
-            default_value="0.6"
-        ),
-
-        DeclareLaunchArgument(
-            name='sim',
-            default_value='true',
-            description='Enable use_sime_time to true'
-        ),
-
-        DeclareLaunchArgument(
-            name='hardware_connected',
-            default_value='false',
-            description='Set to true if connected to a physical robot'
-        ),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(champ_bringup_path),
-            launch_arguments={
-                "use_sim_time": LaunchConfiguration("sim"),
-                "robot_name": LaunchConfiguration("robot_name"),
-                "gazebo": LaunchConfiguration("sim"),
-                "rviz": LaunchConfiguration("rviz"),
-                "hardware_connected": LaunchConfiguration("hardware_connected"),
-                "publish_foot_contacts": "true",
-                "close_loop_odom": "true",
-                "joint_controller_topic": "joint_group_effort_controller/joint_trajectory",
-                "joints_map_path": joints_config_path,
-                "links_map_path": links_config_path,
-                "gait_config_path": gait_config_path,
-                "description_path": description_path,
-                "ros_control_file": ros_control_config_path
-            }.items(),
-        ),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(
-                get_package_share_directory("champ_gazebo"), "launch", "gazebo.launch.py")),
-            launch_arguments={
-                "use_sim_time": LaunchConfiguration("use_sim_time"),
-                "robot_name": LaunchConfiguration("robot_name"),
-                "world": LaunchConfiguration("world"),
-                "lite": LaunchConfiguration("lite"),
-                "world_init_x": LaunchConfiguration("world_init_x"),
-                "world_init_y": LaunchConfiguration("world_init_y"),
-                "world_init_heading": LaunchConfiguration("world_init_heading"),
-                "gui": LaunchConfiguration("gui"),
-                "close_loop_odom": "true",
-            }.items(),
-        )
+        declare_rviz,
+        declare_robot_name,
+        declare_lite,
+        declare_ros_control_file,
+        declare_world,
+        declare_gui,
+        declare_world_init_x,
+        declare_world_init_y,
+        declare_world_init_heading,
+        declare_sim,
+        declare_hardware_connected,
+        mini_pupper_bringup_launch,
+        champ_gazebo_launch
     ])
