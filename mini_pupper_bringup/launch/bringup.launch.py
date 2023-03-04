264e1bee0c6ca4720f13ebde7a6e97483d723c31
Daisuke Sato <tiryoh@gmail.com>
2023-03-04 17:57:46 +0900

    Remove unused package

--- mini_pupper_bringup/launch/bringup.launch.py
+++ mini_pupper_bringup/launch/bringup.launch.py
@@ -26,7 +26,7 @@
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
