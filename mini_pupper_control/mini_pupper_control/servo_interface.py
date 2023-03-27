9f9d17aec339a2f0aee238a4bc75715c9057fa07
HermanYe <hermanye233@icloud.com>
2023-03-27 10:06:23 +0800

    Revert "Update for mini_pupper_control"
    This reverts commit 0c5bb5d8526f43dd3b8fbeea50ff928ee9e04ba1.

--- mini_pupper_control/mini_pupper_control/servo_interface.py
+++ mini_pupper_control/mini_pupper_control/servo_interface.py
@@ -20,7 +20,7 @@
 import numpy as np
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory
 from MangDang.mini_pupper.HardwareInterface import HardwareInterface
 
 
@@ -28,13 +28,33 @@ class ServoInterface(Node):
     def __init__(self):
         super().__init__('servo_interface')
         self.subscriber = self.create_subscription(
-            JointState, '/joint_command',
+            JointTrajectory, '/joint_group_effort_controller/joint_trajectory',
             self.cmd_callback, 1)
         self.hardware_interface = HardwareInterface()
 
     def cmd_callback(self, msg):
-        joint_angles = np.array(msg.position).reshape(3, 4)
+        joint_positions = msg.points[0].positions
+        lf1_position = joint_positions[0]
+        lf2_position = joint_positions[1]
+        lf3_position = joint_positions[2]
+        rf1_position = joint_positions[3]
+        rf2_position = joint_positions[4]
+        rf3_position = joint_positions[5]
+        lb1_position = joint_positions[6]
+        lb2_position = joint_positions[7]
+        lb3_position = joint_positions[8]
+        rb1_position = joint_positions[9]
+        rb2_position = joint_positions[10]
+        rb3_position = joint_positions[11]
+
+        joint_angles = np.array([
+            [rf1_position, lf1_position, rb1_position, lb1_position],
+            [rf2_position, lf2_position, rb2_position, lb2_position],
+            [rf2_position + rf3_position, lf2_position + lf3_position,
+             rb2_position + rb3_position, lb2_position + lb3_position]
+        ])
         self.hardware_interface.set_actuator_postions(joint_angles)
+
 
 def main(args=None):
     rclpy.init(args=args)
