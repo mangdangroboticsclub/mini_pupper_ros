7f9ceb167a3a6aebf3e5385cecbcf470b883753a
HermanYe <hermanye233@icloud.com>
2023-03-30 18:51:41 +0800

    exchange mini_pupper_control & mini_pupper_driver

--- mini_pupper_control/setup.py
+++ mini_pupper_control/setup.py
@@ -1,32 +1,25 @@
 from setuptools import setup
-from glob import glob
-import os
 
 package_name = 'mini_pupper_control'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    author='MangDang',
-    author_email='fae@mangdang.net',
     maintainer='MangDang',
     maintainer_email='fae@mangdang.net',
-    description='The mini_pupper_control package',
-    license='MIT',
+    description='Mini pupper motor control package',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'servo_interface = mini_pupper_control.servo_interface:main',
-            'display_interface = mini_pupper_control.display_interface:main'
         ],
     },
 )
