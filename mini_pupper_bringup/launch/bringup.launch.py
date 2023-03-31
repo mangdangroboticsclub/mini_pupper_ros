127b6ce13396642b168bea5621652402ea6ff58c
HermanYe <hermanye233@icloud.com>
2023-03-31 16:22:13 +0800

    Update bringup.launch.py

--- mini_pupper_bringup/launch/bringup.launch.py
+++ mini_pupper_bringup/launch/bringup.launch.py
@@ -9,6 +9,16 @@
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This program is based on https://github.com/champ/champ.
+# which is released under the Apache-2.0 License.
+# http://www.apache.org/licenses/LICENSE-2.0
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
