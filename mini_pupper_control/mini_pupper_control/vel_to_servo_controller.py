b62f0fa174f40dd03bd457f47b24154cacb88cf5
MangDang <afreez.gan@gmail.com>
2023-04-02 01:20:32 +0800

    Update license info
    Add reference license info

--- mini_pupper_control/mini_pupper_control/vel_to_servo_controller.py
+++ mini_pupper_control/mini_pupper_control/vel_to_servo_controller.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 #
-# SPDX-License-Identifier: Apache-2.0
-#
-# Copyright (c) 2023 MangDang
+# Copyright 2023 MangDang
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# This program is based on https://github.com/stanfordroboticsclub/StanfordQuadruped/blob/master/run_robot.py
+# which is released under the MIT License.
+# Copyright (c) 2020 Stanford Student Robotics
 
 import numpy as np
 # from .StanfordQuadruped.src.IMU import IMU
