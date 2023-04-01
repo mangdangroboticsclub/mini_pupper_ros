2a6dbe1c8a4ede656782f9cc39dd494e7e462564
MangDang <afreez.gan@gmail.com>
2023-04-02 00:39:34 +0800

    Update license format
    same with original license format

--- mini_pupper_control/mini_pupper_control/StanfordQuadruped/pupper/Config.py
+++ mini_pupper_control/mini_pupper_control/StanfordQuadruped/pupper/Config.py
@@ -1,4 +1,5 @@
-# Copyright 2023 stanfordroboticsclub
+# MIT License
+# Copyright (c) 2020 Stanford Student Robotics
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
