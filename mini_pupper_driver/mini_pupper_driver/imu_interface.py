8aff036b02aef0ae4a6fbf9e8df67e0ae7d081d3
MangDang <wanghua19891004@gmail.com>
2023-04-13 11:51:15 +0800

    update the Copyright format
    update the Copyright format

--- mini_pupper_driver/mini_pupper_driver/imu_interface.py
+++ mini_pupper_driver/mini_pupper_driver/imu_interface.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 #
-# SPDX-License-Identifier: Apache-2.0
-#
-# Copyright (c) 2022-2023 MangDang
+# Copyright 2023 MangDang
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
