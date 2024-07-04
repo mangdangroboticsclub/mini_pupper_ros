89c376be5ad4a069be57e126f4a949c57b585ff7
Cullen SUN <sunhongshuai@gmail.com>
2024-07-05 01:13:07 +0800

    remove unused import

--- mini_pupper_bringup/launch/bringup.launch.py
+++ mini_pupper_bringup/launch/bringup.launch.py
@@ -20,7 +20,7 @@ import os
 import yaml
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch.conditions import IfCondition
