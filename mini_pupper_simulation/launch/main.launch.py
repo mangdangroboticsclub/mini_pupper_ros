0229936c9db0eb5632d542668587d0f069a0af4a
Cullen SUN <sunhongshuai@gmail.com>
2024-11-11 01:13:00 +0800

    add license for gazebo launch file

--- mini_pupper_simulation/launch/main.launch.py
+++ mini_pupper_simulation/launch/main.launch.py
@@ -19,7 +19,7 @@
 
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription,LogInfo
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
