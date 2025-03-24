f5896273b0e68a97ecaf6e508c67d9790aac1b88
Cullen SUN <sunhongshuai@gmail.com>
2025-03-25 00:33:01 +0800

    fix typo

--- stanford_controller/stanford_controller/twist_to_command_node.py
+++ stanford_controller/stanford_controller/twist_to_command_node.py
@@ -20,7 +20,7 @@ class TwistToCommandNode(Node):
         command_msg = Command()
         x_vel = min(self.config.max_x_velocity, msg.linear.x)
         y_vel = min(self.config.max_y_velocity, msg.linear.y)
-        yaw_rate = min(self.max_yaw_rate, msg.angular.z)
+        yaw_rate = min(self.config.max_yaw_rate, msg.angular.z)
         command_msg.horizontal_velocity = np.array([x_vel, y_vel])
         command_msg.yaw_rate = yaw_rate
         self.publisher_.publish(command_msg)
