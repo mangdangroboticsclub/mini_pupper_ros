c30c23e2b6e72876bf9a06992a0c507237dc6c26
Cullen SUN <sunhongshuai@gmail.com>
2025-03-25 00:30:15 +0800

    update constraints for the speed

--- stanford_controller/stanford_controller/twist_to_command_node.py
+++ stanford_controller/stanford_controller/twist_to_command_node.py
@@ -1,12 +1,14 @@
 import rclpy
 from rclpy.node import Node
+import numpy as np
 from geometry_msgs.msg import Twist
 from mini_pupper_interfaces.msg import Command
-import numpy as np
+from MangDang.mini_pupper.Config import Configuration
 
 class TwistToCommandNode(Node):
-    def __init__(self):
+    def __init__(self, config):
         super().__init__('twist_to_command_node')
+        self.config = config
         self.publisher_ = self.create_publisher(Command, 'robot_command', 10)
         self.subscription = self.create_subscription(
             Twist,
@@ -16,14 +18,18 @@ class TwistToCommandNode(Node):
 
     def cmd_vel_callback(self, msg):
         command_msg = Command()
-        command_msg.horizontal_velocity = np.array([msg.linear.x, msg.linear.y])
-        command_msg.yaw_rate = msg.angular.z
+        x_vel = min(self.config.max_x_velocity, msg.linear.x)
+        y_vel = min(self.config.max_y_velocity, msg.linear.y)
+        yaw_rate = min(self.max_yaw_rate, msg.angular.z)
+        command_msg.horizontal_velocity = np.array([x_vel, y_vel])
+        command_msg.yaw_rate = yaw_rate
         self.publisher_.publish(command_msg)
         self.get_logger().info(f'Published Command: horizontal_velocity=({command_msg.horizontal_velocity[0]}, {command_msg.horizontal_velocity[1]}), yaw_rate={command_msg.yaw_rate}')
 
 def main(args=None):
     rclpy.init(args=args)
-    node = TwistToCommandNode()
+    config = Configuration()
+    node = TwistToCommandNode(config)
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
