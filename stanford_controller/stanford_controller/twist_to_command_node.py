9cabcf703930a965a0b027cbfee6018571cd2e57
Cullen SUN <sunhongshuai@gmail.com>
2025-04-27 01:03:01 +0800

    add timer for cmd converter

--- stanford_controller/stanford_controller/twist_to_command_node.py
+++ stanford_controller/stanford_controller/twist_to_command_node.py
@@ -8,37 +8,71 @@ from MangDang.mini_pupper.Config import Configuration
 
 
 class TwistToCommandNode(Node):
+
     def __init__(self, config):
         super().__init__('twist_to_command_node')
         self.config = config
+        self.last_twist = self.get_clock().now()
+        self.timer = self.create_timer(self.config.dt, self.timer_callback)
         self.publisher_ = self.create_publisher(Command, 'robot_command', 10)
         self.subscription = self.create_subscription(
             Twist,
             'cmd_vel',
             self.cmd_vel_callback,
             10)
+        self.last_command = None
         self.is_trotting = False
 
+    def timer_callback(self):
+        if self.last_command is not None and \
+            (self.get_clock().now() - self.last_twist) > self.config.dt * 4:
+            # publish the last command
+            self.publisher_.publish(self.last_command )
+            self.get_logger().debug('Published last command')
+        else:
+            # if no fresh /cmd_vel, publish default stand‐still command
+            command = self.get_default_command()
+            self.publisher_.publish(command)
+            self.get_logger().debug('Published stand command')
+
     def cmd_vel_callback(self, msg):
+        self.last_twist = self.get_clock().now()
         command = self.create_command(msg)
-        self.publisher_.publish(command)
         self.get_logger().info(
             f'Published Command: \
-              horizontal_velocity=({command.horizontal_velocity[0]}, \
-              {command.horizontal_velocity[1]}), yaw_rate={command.yaw_rate}')
+              horizontal_velocity=({command.horizontal_velocity}, \
+              yaw_rate={command.yaw_rate}')
         self.get_logger().info(
             f'Published Command: trot_event=({command.trot_event}, \
               self.is_trotting={self.is_trotting}')
+        self.last_command = command
+
+    def get_default_command(self):
+        cmd = Command()
+        cmd.height = -0.07
+        cmd.horizontal_velocity = [0.0, 0.0]
+        matrix = Matrix3x4()
+        matrix.row1 = [0.06, 0.06, -0.06, -0.06]
+        matrix.row2 = [-0.05, 0.05, -0.05, 0.05]
+        matrix.row3 = [-0.07, -0.07, -0.07, -0.07]
+        cmd.legs_location = matrix
+        cmd.yaw_rate = 0.0
+        cmd.roll = 0.0
+        cmd.pitch = 0.0
+        cmd.yaw = 0.0
+        cmd.trot_event = self.is_trotting
+        self.is_trotting = False
+        return cmd
 
     def create_command(self, cmd_vel):
-        command = Command()
-        command.height = -0.07
+        cmd = Command()
+        cmd.height = -0.07
         is_cmd_zero = np.allclose(
             [cmd_vel.linear.x, cmd_vel.linear.y, cmd_vel.angular.z],
             0,
             atol=1e-3
         )
-        command.trot_event = (
+        cmd.trot_event = (
             self.is_trotting and is_cmd_zero) or (
             not self.is_trotting and not is_cmd_zero)
         self.is_trotting = not is_cmd_zero
@@ -48,14 +82,17 @@ class TwistToCommandNode(Node):
         matrix.row1 = [0.06, 0.06, -0.06, -0.06]
         matrix.row2 = [-0.05, 0.05, -0.05, 0.05]
         matrix.row3 = [-0.07, -0.07, -0.07, -0.07]
-        command.legs_location = matrix
+        cmd.legs_location = matrix
 
         x_vel = min(self.config.max_x_velocity, cmd_vel.linear.x)
         y_vel = min(self.config.max_y_velocity, cmd_vel.linear.y)
         yaw_rate = min(self.config.max_yaw_rate, cmd_vel.angular.z)
-        command.horizontal_velocity = np.array([x_vel, y_vel])
-        command.yaw_rate = yaw_rate
-        return command
+        cmd.horizontal_velocity = np.array([x_vel, y_vel])
+        cmd.yaw_rate = yaw_rate
+        cmd.roll = 0.0
+        cmd.pitch = 0.0
+        cmd.yaw = 0.0
+        return cmd
 
 
 def main(args=None):
