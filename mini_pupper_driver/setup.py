16e6d2445df11293fef6fa8855d12ed96edcc9f3
Kishan <kishangrewal06@gmail.com>
2025-09-03 11:30:20 +0800

    feat: add curvature comp

--- mini_pupper_driver/setup.py
+++ mini_pupper_driver/setup.py
@@ -27,7 +27,8 @@ setup(
         'console_scripts': [
             'servo_interface = mini_pupper_driver.servo_interface:main',
             'display_interface = mini_pupper_driver.display_interface:main',
-            'imu_interface = mini_pupper_driver.imu_interface:main'
+            'imu_interface = mini_pupper_driver.imu_interface:main',
+            'curvature_compensation = mini_pupper_driver.curvature_compensation:main',
         ],
     },
 )
