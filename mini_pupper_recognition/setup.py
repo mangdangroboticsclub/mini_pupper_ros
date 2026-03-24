1efd3307883e48ceb78f5acdd6e3493430634efc
Kahfi Elhady <113518657+zamadye@users.noreply.github.com>
2026-03-24 08:12:37 +0800

    fix: upgrade ros2 humble to jazzy

--- mini_pupper_recognition/setup.py
+++ mini_pupper_recognition/setup.py
@@ -5,7 +5,7 @@ package_name = 'mini_pupper_recognition'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
