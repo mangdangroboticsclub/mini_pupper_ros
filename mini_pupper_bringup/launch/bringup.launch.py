05c760a888c09a6fba85f8c8fb2f081114b0f652
Cullen SUN <sunhongshuai@gmail.com>
2026-05-09 22:48:27 +0800

    fix copyright year

--- mini_pupper_bringup/launch/bringup.launch.py
+++ mini_pupper_bringup/launch/bringup.launch.py
@@ -2,7 +2,7 @@
 
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright (c) 2025 MangDang
+# Copyright (c) 2026 MangDang
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
