227c90484e9c2dce41897545739c2de4b594a74b
sunflower050105 <huyduc.ntp@gmail.com>
2026-05-29 05:29:45 +0000

    Change package dir

--- mini_pupper_dance/setup.py
+++ mini_pupper_dance/setup.py
@@ -9,7 +9,7 @@ setup(
     version='0.0.1',
     packages=['mini_pupper_dance'],
     package_dir={
-        'mini_pupper_dance': 'mini_pupper_dance'
+        '': '.'
     },
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
